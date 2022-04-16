0a5d1d7af746a7b5491dd7f81415e7c029ac1f4b
Akhlaque <akhlaquea01@gmail.com>
2022-04-16 12:28:24 +0530

    Added List and Grid View

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  isListView!: boolean;;
+  topicDetails: Array<any> = [
+    {
+      title: 'Angular',
+      description: 'Angular is a platform that makes it easy to build applications with the web.',
+    },
+    {
+      title: 'React',
+      description: 'React is a JavaScript library for building user interfaces.',
+    },
+    {
+      title: 'Vue',
+      description: 'Vue.js is an open-source progressive JavaScript framework for building user interfaces.',
+    },
+    {
+      title: 'Angular',
+      description: 'Angular is a platform that makes it easy to build applications with the web.',
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  listView() { 
+    this.isListView = true;
+  }
+  gridView() { 
+    this.isListView = false;
+  }
 }
