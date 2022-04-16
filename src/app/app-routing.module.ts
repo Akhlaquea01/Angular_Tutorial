ec7f3ede553459b1c63132f91a9998efb8863875
Akhlaque <akhlaquea01@gmail.com>
2022-04-16 13:26:27 +0530

    Added Empty components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutAngularComponent } from './about-angular/about-angular.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
 
 const routes: Routes = [
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
-  {path: 'Link1', component:HomeComponent},
-  {path: 'Link2', component:HomeComponent},
-  {path: 'Link3', component:HomeComponent},
-  {path: 'Link4', component:HomeComponent},
-  {path: 'Link5', component:HomeComponent},
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about-angular', component: AboutAngularComponent },
+  { path: 'blogs', component: BlogsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutAngularComponent } from './about-angular/about-angular.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutUsComponent,
+    AboutAngularComponent,
+    ContactComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
