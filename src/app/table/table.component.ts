a8ae7cada0fc62cbfba0c0109d591ad3c8e9b681
Akhlaque <akhlaquea01@gmail.com>
2022-05-03 19:22:03 +0530

    Working Fine

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -9,7 +9,7 @@ export class TableComponent implements OnInit {
 
   topCol = [{ id: 1, day: "Mon" }, { id: 2, day: "Tue" }, { id: 3, day: "Wed" }]
   leftCol = [{ id: 'a', date: "1" }, { id: 'b', date: "2" }, { id: 'c', date: "3" }]
-  data=[{id:'1$a',val:21},{id:'1$b',val:22},{id:'1$c',val:23},{id:'2$a',val:24},{id:'2$b',val:25},{id:'2$c',val:26},{id:'3$a',val:27},{id:'3$b',val:28},{id:'3$c',val:29}]
+  data: any = { '1$a': { id: '1$$a', val: 21 }, '1$b': { id: '1$$b', val: 22 }, '1$c': { id: '1$$c', val: 23 }, '2$a': { id: '2$$a', val: 24 }, '2$b': { id: '2$$b', val: 25 }, '2$c': { id: '2$$c', val: 26 }, '3$a': { id: '3$$a', val: 27 }, '3$b': { id: '3$$b', val: 28 }, '3$c': { id: '3$$c', val: 29 } }
   header = ['BLANK', ...this.topCol];
 
   rows: any[] = [];
@@ -28,8 +28,13 @@ export class TableComponent implements OnInit {
 
       this.topCol.forEach((ele, i) => {
         const key = ele.id + '$' + el.id;
-        this.data.forEach(data => {
-          if (data.id == key) {
+        var myData = Object.keys(this.data).map(key => {
+          this.data[key].Atts=key
+          return this.data[key];
+        })
+        console.log(myData);
+        myData.forEach(data => {
+          if (data.Atts == key) {
             row.push(data);
           }
         })
@@ -40,7 +45,7 @@ export class TableComponent implements OnInit {
 
     this.rows = rows;
   }
-ngOnInit(): void {
-    
-}
+  ngOnInit(): void {
+
+  }
 }
